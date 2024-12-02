fa7b89182503cb2ba73643acf03122013e5f1dbf
Nadiia <zoryvchaknv@gmail.com>
2024-12-02 16:44:54 +0200

    make build

---
+++ index.4a4a9a6b.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=e.clientWidth/2-t.clientWidth/2,l=e.clientHeight/2-t.clientHeight/2;t.style.top="".concat(c,"px"),t.style.left="".concat(l,"px");
+//# sourceMappingURL=index.4a4a9a6b.js.map
